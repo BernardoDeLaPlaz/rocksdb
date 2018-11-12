7b43f92473a5de44ed62d850e204c6adb1623423
BernardoDeLaPlaz <tjic_BernardoDeLaPlaz_github@tjic.com>
2018-11-12 18:36:36 -0500

    more meson

---
+++ util/build_version.cc
@@ -0,0 +1,4 @@
+#include "build_version.h"
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:0148f717ff7e538df0644c7dfc5c53ff95d72a12";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:2018-11-08";
+const char* rocksdb_build_compile_date = __DATE__;
